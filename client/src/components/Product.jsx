207fe2b5c82ab69c82c67f801248833475e1b190
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-30 14:30:15 +0530

    Update Product.jsx

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 
 export const Product = ({ product }) => {
     const [varients,setVarients] = useState("small")
-    const [varients,setVarients] = useState("small")
+    const [quantity,setQuantity] = useState(10)
   return (
     <Card className="mt-3 mb-5" style={{ width: '18rem' }}>
       <Card.Img variant="top" src={product.image} alt={product.name} />
@@ -16,9 +16,9 @@ export const Product = ({ product }) => {
             <Card.Text>
               <strong>Vardents:</strong>
             </Card.Text>
-            <select className="form-select">
+            <select className="form-select" value={varients} onChange={(e) => setVarients(e.target.value)}>
               {product.varients.map(vardent => (
-                <option key={vardent} value={vardent}>
+                <option key={vardent} >
                   {vardent}
                 </option>
               ))}
@@ -28,7 +28,7 @@ export const Product = ({ product }) => {
             <Card.Text>
               <strong>Quantity:</strong>
             </Card.Text>
-            <select className="form-select">
+            <select className="form-select" value={quantity} onChange={(e) => setQuantity(e.target.value)}>
             {Array.from({ length: 91 }, (_, i) => i + 9).map(i => (
                 <option key={i} value={i + 1}>
                   {i + 1}
@@ -37,9 +37,10 @@ export const Product = ({ product }) => {
             </select>
           </Col>
         </Row>
-        <NavLink to="#" className="btn btn-primary mt-3">
-          Go somewhere
-        </NavLink>
+            <Card.Text>
+                <Col  md={6}> price: {product.prices[0][varients] * quantity}
+                </Col>
+            </Card.Text>
       </Card.Body>
     </Card>
   );
