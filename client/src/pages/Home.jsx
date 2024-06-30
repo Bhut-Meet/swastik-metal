2790084a16d7f60ce0541d877952220aea4b29e1
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-30 14:40:16 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,7 +9,7 @@ export const Home = () => {
         <Row>
           {
             AllProducts.map(product => (
-              <Col key={product.id} xs={12} sm={6} md={4} lg={3}>
+              <Col key={product.id} xs={6} sm={6} md={4} lg={3}>
             <Product product={product} />
           </Col>
             ))
