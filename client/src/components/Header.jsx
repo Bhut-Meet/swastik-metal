ec4c0e5936329b8feba0c1fbfba890f208eb1bf1
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-30 15:07:30 +0530

    new

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,32 +1,76 @@
 import { Link } from "react-router-dom";
-import "../components/header.css"
+import "../components/header.css";
+import { useState, useEffect } from "react";
+
 export const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isClosing, setIsClosing] = useState(false);
+
+  const toggleNavbar = () => {
+    if (isOpen) {
+      setIsClosing(true);
+      setTimeout(() => {
+        setIsOpen(false);
+        setIsClosing(false);
+      }, 300); // This should match the transition duration in CSS
+    } else {
+      setIsOpen(true);
+    }
+  };
+
+  const closeNavbar = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsOpen(false);
+      setIsClosing(false);
+    }, 300); // This should match the transition duration in CSS
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth > 992 && isOpen) {
+        setIsOpen(false);
+        setIsClosing(false);
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, [isOpen]);
+
   return (
-    <header className="navbar navbar-expand-lg  fixed-top ">  {/* Use custom class for background color */}
-      <div className="container my-3  bg-custom rounded-5 custom-nav">  {/* Removed redundant bg-danger class */}
-        <Link to="#" className="navbar-brand" href="#">Swastik Metal</Link>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <header className="navbar navbar-expand-lg fixed-top">
+      <div className="container my-3 bg-custom rounded-5 custom-nav">
+        <Link to="#" className="navbar-brand">Swastik Metal</Link>
+        <button 
+          className="navbar-toggler" 
+          type="button" 
+          onClick={toggleNavbar}
+          aria-controls="navbarNav" 
+          aria-expanded={isOpen} 
+          aria-label="Toggle navigation"
+        >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-          <ul className="navbar-nav text-white">  {/* Set text color to white using a common class */}
-            <li className="nav-item ">
-              <Link to="/" className="nav-link " >Home</Link>
+        <div className={`collapse navbar-collapse justify-content-end ${isOpen ? 'show' : ''} ${isClosing ? 'closing' : ''}`}>
+          <ul className="navbar-nav text-white">
+            <li className="nav-item">
+              <Link to="/" className="nav-link" onClick={closeNavbar}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/product" className="nav-link" >Products</Link>
+              <Link to="/product" className="nav-link" onClick={closeNavbar}>Products</Link>
             </li>
             <li className="nav-item">
-              <Link to="#" className="nav-link" >About</Link>
+              <Link to="#" className="nav-link" onClick={closeNavbar}>About</Link>
             </li>
             <li className="nav-item">
-              <Link to="#" className="nav-link" >Contact</Link>
+              <Link to="#" className="nav-link" onClick={closeNavbar}>Contact</Link>
             </li>
             <li className="nav-item">
-              <Link to="/login" className="nav-link" >Login</Link>
+              <Link to="/login" className="nav-link" onClick={closeNavbar}>Login</Link>
             </li>
             <li className="nav-item">
-              <Link to="/register" className="nav-link" >Register</Link>
+              <Link to="/register" className="nav-link" onClick={closeNavbar}>Register</Link>
             </li>
           </ul>
         </div>
